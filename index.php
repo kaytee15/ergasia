e5818b624ac4c37a895518c6ea0cd3edc6c25568
kaytee15 <77791152+kaytee15@users.noreply.github.com>
2021-11-22 16:51:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<h1>
+
+Get Method<br/>
+
+<hr>
+
+<form action="" method="POST">
+
+<input type="text" placeholder="EPELEKSE ARXIO VLAKA" name="EPELEKSE ARXIO VLAKA">
+<input type="file" placeholder="choose file" >
+
+<button type="submit">UPLODE VLAKA
+
+</button>
+
+</form>
+<pre>
+
+</body>
+</html>
