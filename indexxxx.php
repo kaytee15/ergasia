ea1bb1438efb450ed41fa98d0f19ae6ad9e613c6
kaytee15 <77791152+kaytee15@users.noreply.github.com>
2022-01-14 18:46:26 +0200

    Add files via upload

---
+++ indexxxx.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title >   ΑΝΑΚΟΙΝΩΣΕΙΣ Tμήματος Ψηφιακών Συστημάτων
+    </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+   .mlk{
+    display: inline-block;
+    color: rgb(255, 255, 255);
+   }
+   a{
+        background-color: brown;
+        padding: 1% 5% 1% 5%;
+        border-radius: 40px;
+        border: 4px solid black;  
+        font-size: 1em;
+    }
+    img{
+text-align: center;     
+   max-width: 80%;
+        max-height: 80%;
+        margin: 2.5% 50% 2.5% 27% ;
+        display: inline-block;
+
+    }
+        .announcement {
+  display: inline-block;
+  height: 100%;
+  text-align: center;
+  width: 100%;
+  background: black;
+  color: white;
+  font-family: Arial, sans;
+  margin: 0;
+  padding: 0;
+  vertical-align: middle;
+  font-size: 0.8em;
+}
+
+.announcement a { 
+  color: #ccc; 
+}
+
+.announcement a:hover {
+  color: red;
+}
+    .kkk{   
+         margin: 0.5% 31.5% ;
+        background-color: brown;
+        padding: 1% 4% 0% 4%;  
+    } 
+
+    .k{     
+    background-color: brown;  
+    background-size: cover;
+    border: 8px solid black;  font-size: 1.2em;
+    display:inline-block ;
+    margin: 1% 42.5% 1% 40.5%;
+    padding: 1% 4% 0% 4%;  
+    text-align: center;}    
+
+
+.skr{
+    background-color: rgba(0, 3, 8, 0.9263);
+    margin: 35% 20px 5% 10%;
+    
+}
+body{
+        background-image: url(fonto3.png);
+        background-attachment: fixed;
+        background-repeat:repeat;
+        background-size: 15%;
+        background-position: center;
+        
+    }
+
+h1{
+     font-size: 2.3em;   
+     background-color: rgba(0, 3, 8, 0.9263);
+     color: rgb(255, 255, 255); 
+     font-weight: 30px;
+     text-align: center;
+
+     border-radius: 40px;
+     border: 8px solid rgb(0, 0, 0);
+    margin: 20px 30.5% 0% ;
+    display:inline-block ;
+
+    padding: 0% 10% 0% 10%;
+}
+.logo {
+    display: inline-block;
+    margin-left: auto;
+    margin-right: auto;
+    height: 30px; 
+    text-align: center;
+    max-height: 99%;
+
+
+    display: inline-block;
+}
+#images{
+   text-align:center;}
+
+    </style>
+</head>
+<body>
+<div>
+    
+    <h1>
+        <b>
+            Tμήμα Ψηφιακών Συστημάτων Σπάρτης
+         </b>
+    </h1>
+<br>
+<br>
+</div>
+<div id="images">
+   
+    <a href="indexxx.php"> <b class="mlk"> pata ayto ama eisai kathigitis --></b>
+    <img class="logo"  alt="" src="kath2.jpg"/></a>
+
+  <a href="indexxxxx.php" >  <b class="mlk"> pata auto ama eisai foititis--></b>
+        <img class="logo"  alt="" src="foit2.jpg"/></a></div>
+</html>
---
+++ indexxxxx.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title >   ΑΝΑΚΟΙΝΩΣΕΙΣ Tμήματος Ψηφιακών Συστημάτων
+    </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+    .nn{
+        background-color: brown;
+        margin: 0% 60% 0% 30% ;
+        padding: 1% 5% 1% 5%;
+        border-radius: 40px;
+        display:inline-block ;
+
+    }
+    img{
+text-align: center;     
+   max-width: 80%;
+        max-height: 80%;
+        margin: 0% 50% 0% 27% ;
+
+    }
+        .announcement {
+  display: inline-block;
+  height: 100%;
+  text-align: center;
+  width: 100%;
+  background: black;
+  color: white;
+  font-family: Arial, sans;
+  margin: 0;
+  padding: 0;
+  vertical-align: middle;
+  font-size: 0.8em;
+}
+
+.announcement a { 
+  color: #ccc; 
+}
+
+.announcement a:hover {
+  color: red;
+}
+    .kkk{   
+         margin: 0.5% 31.5% ;
+        background-color: brown;
+        padding: 1% 4% 0% 4%;  
+    } 
+
+    .k{     
+    background-color: brown;  
+    background-size: cover;
+    border: 8px solid black;  font-size: 1.2em;
+    display:inline-block ;
+    margin: 1% 42.5% 0% 40.5%;
+    padding: 1% 4% 0% 4%;  
+    text-align: center;}    
+
+
+.skr{
+    background-color: rgba(0, 3, 8, 0.9263);
+    margin: 35% 20px 5% 10%;
+    
+}
+body{
+        background-image: url(fonto3.png);
+        background-attachment: fixed;
+        background-repeat:repeat;
+        background-size: 15%;
+        background-position: center;
+        
+    }
+
+h1{
+     font-size: 2.3em;   
+     background-color: rgba(0, 3, 8, 0.9263);
+     color: rgb(255, 255, 255); 
+     font-weight: 30px;
+     text-align: center;
+
+     border-radius: 40px;
+     border: 8px solid rgb(0, 0, 0);
+    margin: 20px 30.5% 0% ;
+    display:inline-block ;
+
+    padding: 0% 10% 0% 10%;
+}
+.fblogo {
+    display: inline-block;
+    margin-left: auto;
+    margin-right: auto;
+    height: 100px; 
+}
+#images{
+   text-align:center;
+
+}
+.pap{
+    margin: 20px;
+padding: 0% 50%;
+background-color: brown;
+color: rgb(255, 255, 255); 
+font-size: 20px;
+
+}
+    </style>
+</head>
+<body>
+<div>
+    
+    <h1>
+        <b>
+            Tμήμα Ψηφιακών Συστημάτων Σπάρτης
+         </b>
+    </h1>
+
+</html>
+
+
+<b><form action="indexxxx.php"> <button class="pap" type="submit"> back to home  </button></form></b>
+    
